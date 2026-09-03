176f28d050327c855e80f1ffb40398a12fc2f67a
Nagaraju Thota <nagaraju.t@koundinyasatech.com>
2026-09-03 18:08:06 +0530

    3.5

--- Day-02-docker-image-scan/app.js
+++ Day-02-docker-image-scan/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const helmet = require("helmet");
 
 const app = express();
 
+app.use(helmet());
+
 app.get("/", (req, res) => {
-    res.send("Hello from Trivy!");
+  res.send("Hello Trivy");
+});
+
+app.get("/ping", (req, res) => {
+  res.send("Ping endpoint");
 });
 
 app.listen(3000, () => {
-    console.log("Application running on port 3000");
+  console.log("Server running on port 3000");
 });
